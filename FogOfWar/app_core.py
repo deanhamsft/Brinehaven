f13e5ed5c586b344f11a5391aa3440ad3739872c
deanhamsft <dean888@msn.com>
2026-07-23 15:15:56 -0600

    adding forward button

--- FogOfWar/app_core.py
+++ FogOfWar/app_core.py
@@ -867,6 +867,11 @@ def control_window(initial_image_path, shared_revealed, shared_running, shared_i
         pygame.draw.rect(screen, (100, 140, 200), back_button_rect, border_radius=6)
         back_txt = menu_font.render("Back", True, (255,255,255))
         screen.blit(back_txt, (back_button_rect.x + 25, back_button_rect.y + 5))
+
+        pygame.draw.rect(screen, (200, 140, 200), forward_button_rect, border_radius=6)
+        forward_txt = menu_font.render("Forward", True, (255,255,255))
+        screen.blit(forward_txt, (forward_button_rect.x + 10, forward_button_rect.y + 5))
+
         if backstack:
             count_txt = menu_font.render(f"({len(backstack)})", True, (200, 220, 255))
             screen.blit(count_txt, (back_button_rect.right + 8, back_button_rect.y + 5))
